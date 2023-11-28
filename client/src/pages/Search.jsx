995991f73792c42f691428e0a346731da55dcae1
Damien <damienambient@gmail.com>
2023-11-28 22:36:14 +0200

    created search onSubmit form

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,9 @@
+import { useEffect } from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Search() {
+    const navigate = useNavigate();
     const [sidebardata, setSidebardata] = useState({
         searchTerm: "",
         type: "all",
@@ -10,8 +13,48 @@ export default function Search() {
         sort: "created_at",
         order: "desc",
     });
+    const [loading, setLoading] = useState(false);
+    const [listings, setListings] = useState([]);
+    console.log(listings);
 
-    console.log(sidebardata);
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get("searchTerm");
+        const typeFromUrl = urlParams.get("type");
+        const parkingFromUrl = urlParams.get("parking");
+        const furnishedFromUrl = urlParams.get("furnished");
+        const offerFromUrl = urlParams.get("offer");
+        const sortFromUrl = urlParams.get("sort");
+        const orderFromUrl = urlParams.get("order");
+
+        if (
+            searchTermFromUrl ||
+            typeFromUrl ||
+            parkingFromUrl ||
+            furnishedFromUrl ||
+            offerFromUrl ||
+            sortFromUrl ||
+            orderFromUrl
+        ) {
+            setSidebardata({
+                searchTerm: searchTermFromUrl || "",
+                type: typeFromUrl || "all",
+                parking: parkingFromUrl === "true" ? true : false,
+                furnished: furnishedFromUrl === "true" ? true : false,
+                offer: offerFromUrl === "true" ? true : false,
+                sort: sortFromUrl || "created_at",
+                order: orderFromUrl || "desc",
+            });
+        }
+
+        const fetchListings = async () => {
+            setLoading(true);
+            const searchQuery = urlParams.toString();
+            const res = await fetch(`/api/listing/get?${searchQuery}`);
+            setListings(data);
+            setLoading(false);
+        };
+    }, [location.search]);
 
     const handleChange = (e) => {
         if (
@@ -49,10 +92,24 @@ export default function Search() {
         }
     };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams();
+        urlParams.set("searchTerm", sidebardata.searchTerm);
+        urlParams.set("type", sidebardata.type);
+        urlParams.set("parking", sidebardata.parking);
+        urlParams.set("furnished", sidebardata.furnished);
+        urlParams.set("offer", sidebardata.offer);
+        urlParams.set("sort", sidebardata.sort);
+        urlParams.set("order", sidebardata.order);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
+
     return (
         <div className="flex flex-col md:flex-row md:min-h-screen">
             <div className="p-7 border-b-2 md:border-r-2">
-                <form className="flex flex-col gap-8">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-8">
                     <div className="flex items-center gap-2">
                         <label className="whitespace-nowrap font-semibold">
                             Search Term
