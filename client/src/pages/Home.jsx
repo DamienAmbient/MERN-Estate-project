4547e6fb0381c95bc100d87db7acc6166b1d6e71
Damien <damienambient@gmail.com>
2023-12-02 22:59:15 +0200

    added showlastOffer slider

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import SwiperCore from "swiper";
+import "swiper/css/bundle";
 
 export default function Home() {
     const [offerListings, setOfferListings] = useState([]);
     const [saleListings, setSaleListings] = useState([]);
     const [rentListings, setRentListings] = useState([]);
-    console.log(saleListings);
+    SwiperCore.use([Navigation]);
+    console.log(offerListings);
 
     useEffect(() => {
         const fetchOfferListings = async () => {
@@ -62,6 +67,22 @@ export default function Home() {
                     Let's get started...
                 </Link>
             </div>
+
+            <Swiper navigation>
+                {offerListings &&
+                    offerListings.length > 0 &&
+                    offerListings.map((listing) => (
+                        <SwiperSlide key={listing._id}>
+                            <div
+                                style={{
+                                    background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                                    backgroundSize: "cover",
+                                }}
+                                className="h-[500px]"
+                            ></div>
+                        </SwiperSlide>
+                    ))}
+            </Swiper>
         </div>
     );
 }
