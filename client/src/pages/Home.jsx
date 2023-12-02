152aba648931c31512feabcbf2dc8e42663b4b01
Damien <damienambient@gmail.com>
2023-12-02 22:40:30 +0200

    added showlastOffer logic

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,67 @@
-import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-    return <div>Home</div>;
+    const [offerListings, setOfferListings] = useState([]);
+    const [saleListings, setSaleListings] = useState([]);
+    const [rentListings, setRentListings] = useState([]);
+    console.log(saleListings);
+
+    useEffect(() => {
+        const fetchOfferListings = async () => {
+            try {
+                const res = await fetch("/api/listing/get?offer=true&limit=4");
+                const data = await res.json();
+                setOfferListings(data);
+                fetchRentListings();
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        const fetchRentListings = async () => {
+            try {
+                const res = await fetch("/api/listing/get?type=rent&limit=4");
+                const data = await res.json();
+                setRentListings(data);
+                fetchSaleListings();
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        const fetchSaleListings = async () => {
+            try {
+                const res = await fetch("/api/listing/get?type=sale&limit=4");
+                const data = await res.json();
+                setSaleListings(data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchOfferListings();
+    }, []);
+
+    return (
+        <div>
+            <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+                <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+                    Find your next{" "}
+                    <span className="text-slate-500">perfect</span>
+                    <br />
+                    place with ease
+                </h1>
+                <div className="text-gray-400 text-xs  sm:text-sm">
+                    Damien Estate some text should be here
+                </div>
+                <Link
+                    to={"/search"}
+                    className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
+                >
+                    Let's get started...
+                </Link>
+            </div>
+        </div>
+    );
 }
