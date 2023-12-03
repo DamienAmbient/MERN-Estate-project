370e44398e65c9b68eccbc19eb85f1b0bda90652
Damien <damienambient@gmail.com>
2023-12-03 22:53:40 +0200

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,21 @@
-import React from "react";
-
 export default function About() {
-    return <div>About</div>;
+    return (
+        <div className="py-20 px-4 max-w-6xl mx-auto">
+            <h1 className="text-3xl font-bold mb-4 text-slate-800">
+                About DamienEstate
+            </h1>
+            <p className="mb-4 text-slate-800">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+                quisquam doloremque repellat beatae, explicabo sint laudantium
+                possimus nesciunt at corporis quo sit ratione optio minus
+                assumenda saepe id quam dignissimos!
+            </p>
+            <p className="mb-4 text-slate-800">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+                quidem, deserunt ex alias qui, maiores ut eligendi, distinctio
+                repellat magnam explicabo ullam? Tempora, neque aut.
+                Voluptatibus cumque ex tempore dolor.
+            </p>
+        </div>
+    );
 }
