f70143cfddc0caed03edb08fb310eec8f687e606
Damien <damienambient@gmail.com>
2023-12-09 15:05:36 +0200

    fixed small interface bugs

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "@firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
@@ -17,7 +17,7 @@ export default function OAuth() {
             const res = await fetch("/api/auth/google", {
                 method: "POST",
                 headers: {
-                    "Content-type": "application/json",
+                    "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
                     name: result.user.displayName,
@@ -29,17 +29,16 @@ export default function OAuth() {
             dispatch(signInSuccess(data));
             navigate("/");
         } catch (error) {
-            console.log("no google", error);
+            console.log("could not sign in with google", error);
         }
     };
-
     return (
         <button
             onClick={handleGoogleClick}
             type="button"
             className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
         >
-            Continue with Google
+            Continue with google
         </button>
     );
 }
