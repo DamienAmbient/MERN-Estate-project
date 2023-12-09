b682dc3150d48319dc5dbe5429e2a4b407d29713
Damien <damienambient@gmail.com>
2023-12-09 22:45:27 +0200

    signIn name fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-import SignIn from "./pages/Signin";
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Listing from "./pages/Listing";
 import Search from "./pages/Search";
